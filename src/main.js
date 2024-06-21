8ff66959d3a52f2f375b4399cc17c76ec548b11b
Chase Meadors <c.ed.mead@gmail.com>
2024-06-20 22:14:39 -0600

    Add plausible

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import './styles/flex.css';
 import './styles/main.css';
 import App from './App.svelte';
 
+import Plausible from 'plausible-tracker';
+Plausible({
+    domain: 'cemulate.github.io/ootmmr-checklist',
+    apiHost: 'https://plausible.351321.xyz',
+}).enableAutoPageviews();
+
 const app = new App({
     target: document.getElementById('app'),
 });
